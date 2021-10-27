477cbea4f7d0e94a1e26b1318790d17c2351e037
magicjar <9734293+magicjar@users.noreply.github.com>
2021-10-27 17:49:58 +0700

    Split game data editor script into it's own file

---
+++ Editor/GameData/GameDataEditor.cs
@@ -0,0 +1,31 @@
+using UnityEditor;
+using System.Linq;
+using System.IO;
+
+public class GameDataEditor : Editor
+{
+    // [MenuItem("Agraris/Game Data/Save", false, 50)]
+    static void SaveGameData()
+    {
+        GameData.Init();
+        GameData.Save();
+    }
+
+    [MenuItem("Agraris/Game Data/Clear", false, 51)]
+    static void ClearGameData()
+    {
+        GameData.DeleteSave();
+    }
+
+    [MenuItem("Agraris/Game Data/Open in Explorer", false, 52)]
+    static void OpenGameDataInExplorer()
+    {
+        string path = Directory.CreateDirectory(GameData.SaveDirectory).FullName;
+        string file = Directory.EnumerateFiles(path).FirstOrDefault();
+
+        if (!string.IsNullOrEmpty(file))
+            EditorUtility.RevealInFinder(Path.Combine(path, file));
+        else
+            EditorUtility.RevealInFinder(path);
+    }
+}
--- Runtime/GameData/GameData.cs
+++ Runtime/GameData/GameData.cs
@@ -2,8 +2,6 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEditor;
-using System.Linq;
 using System.Text;
 using Newtonsoft.Json;
 
@@ -286,33 +284,3 @@ public static class GameData
     }
     #endregion
 }
-
-#if UNITY_EDITOR
-public class GameDataEditor : Editor
-{
-    // [MenuItem("Agraris/Game Data/Save", false, 50)]
-    static void SaveGameData()
-    {
-        GameData.Init();
-        GameData.Save();
-    }
-
-    [MenuItem("Agraris/Game Data/Clear", false, 51)]
-    static void ClearGameData()
-    {
-        GameData.DeleteSave();
-    }
-
-    [MenuItem("Agraris/Game Data/Open in Explorer", false, 52)]
-    static void OpenGameDataInExplorer()
-    {
-        string path = Directory.CreateDirectory(GameData.SaveDirectory).FullName;
-        string file = Directory.EnumerateFiles(path).FirstOrDefault();
-
-        if (!string.IsNullOrEmpty(file))
-            EditorUtility.RevealInFinder(Path.Combine(path, file));
-        else
-            EditorUtility.RevealInFinder(path);
-    }
-}
-#endif
